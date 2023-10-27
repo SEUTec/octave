0b15d8dbc1418e57d2159ee3f4c4621f20052cf9
SEUTec <sje@tinet.org>
2023-10-27 18:31:55 +0200

    Initial commit

---
+++ 00_hello.m
@@ -0,0 +1,12 @@
+#! /usr/bin/octave -qf --persist
+# a sample Octave program
+#
+# if this file is execuable, and octave is installed in the beginning path
+#
+# rutn this file with:  ./00_hello.m
+#
+# else: octave 00_hello.m
+#
+
+
+printf ("Hello, world!\n");
